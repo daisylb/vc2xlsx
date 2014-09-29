b1f00f2c93afc814efef4993bae67b7bdca3dbab
Adam Brenecki <adam@brenecki.id.au>
2014-09-29 13:05:19 +0930

    test(cell_parser): Add basic test suite

---
+++ test_cell_parser.py
@@ -0,0 +1,27 @@
+import parser
+
+def do_test(inv, outv):
+    try:
+        tree = parser.parse(inv)
+        actual_output = tree.excel()
+    except Exception as e:
+        print("ERROR")
+        print(e)
+    else:
+        if actual_output != outv:
+            print("FAIL")
+            print("Input          : {}".format(inv))
+            print("Expected output: {}".format(outv))
+            print("Actual output  : {}".format(actual_output))
+            print("Tree           : {}".format(repr(tree)))
+
+
+# Labels
+do_test('\'hello', '\'hello')
+do_test('"hello', 'hello')
+do_test('hello', 'hello')
+do_test('HELLO', 'HELLO')
+do_test('=1+1', '\'=1+1')
+
+do_test('((1))', '1')
+do_test('(1+2', '=1+2')
